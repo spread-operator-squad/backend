f8ff11aed11f902d3323b059127ba90c92959d07
Wahyudin <whydin.id@hotmail.com>
2019-12-19 12:44:52 +0700

    [FEATURE] user able to create an account

--- src/main/java/com/enigma/controller/UserController.java
+++ src/main/java/com/enigma/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.enigma.controller;
 
+import com.enigma.constans.RoleConstants;
 import com.enigma.entities.User;
 import com.enigma.services.CustomResponse;
 import com.enigma.services.UserService;
@@ -8,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -15,30 +18,35 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @RolesAllowed(RoleConstants.ROLE_ADMINISTRATOR)
     @GetMapping
     public ResponseEntity<CustomResponse> getAllUser(){
         CustomResponse response = this.userService.findAll();
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
+    @RolesAllowed(RoleConstants.ROLE_ADMINISTRATOR)
     @GetMapping("/{id}")
     public ResponseEntity<CustomResponse> getUserById(@PathVariable String id){
         CustomResponse response = this.userService.findUserById(id);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
+    @RolesAllowed(RoleConstants.ROLE_ADMINISTRATOR)
     @PostMapping
     public ResponseEntity<CustomResponse> saveUser(@RequestBody User user){
         CustomResponse response = this.userService.saveUser(user);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
+    @RolesAllowed(RoleConstants.ROLE_ADMINISTRATOR)
     @PutMapping
     public ResponseEntity<CustomResponse> updateUser(@RequestBody User user){
         CustomResponse response = this.userService.updateUser(user);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
     }
 
+    @RolesAllowed(RoleConstants.ROLE_ADMINISTRATOR)
     @DeleteMapping("/{id}")
     public ResponseEntity<CustomResponse> deleteUserByID(@PathVariable String id){
         CustomResponse response = this.userService.deleteUserById(id);
--- src/main/java/com/enigma/services/UserServiceImpl.java
+++ src/main/java/com/enigma/services/UserServiceImpl.java
@@ -1,17 +1,30 @@
 package com.enigma.services;
 
 import com.enigma.constans.ResponseMessageConstants;
+import com.enigma.entities.Role;
 import com.enigma.entities.User;
+import com.enigma.entities.UserDetail;
+import com.enigma.enumeration.UserRoles;
+import com.enigma.repositories.RoleRepository;
 import com.enigma.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    RoleService roleService;
 
     @Override
     public CustomResponse findAll(){
@@ -20,7 +33,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public CustomResponse saveUser(User user){
+        if (!(user.getUserRoles().isEmpty())) user.setUserRoles(userHasRole(user));
+        if (user.getUserDetail() != null) user.getUserDetail().setUser(user);
         return new CustomResponse(new Status(HttpStatus.CREATED,ResponseMessageConstants.SUCCESS_SAVE_USER), this.userRepository.save(user));
+    }
+
+    private Set<Role> userHasRole(User user) {
+        Set<Role> roles = new HashSet<>();
+        if (!(user.getRoles().isEmpty())) {
+            for (String role:user.getRoles()){
+                roles.add(roleService.getRoleByRoles(UserRoles.getUserRoleByLabel(role)));
+            }
+        }
+        return roles;
     }
 
     @Override
