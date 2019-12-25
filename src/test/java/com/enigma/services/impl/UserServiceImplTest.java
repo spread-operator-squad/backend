0508fdf9e8cab387b61d8d8961b53619431c3c1f
Wahyudin <whydin.id@hotmail.com>
2019-12-25 18:32:19 +0700

    [TEST] Add unit testing for User Service

--- src/test/java/com/enigma/services/impl/UserServiceImplTest.java
+++ src/test/java/com/enigma/services/impl/UserServiceImplTest.java
@@ -1,7 +1,8 @@
-package com.enigma.services;
+package com.enigma.services.impl;
 
 import com.enigma.entities.User;
 import com.enigma.repositories.UserRepository;
+import com.enigma.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -36,9 +39,14 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findUserById_should_call_userRepository_findById_once() {
+    void findUserById_should_call_userRepository_findById_twice() {
+        User sampleUser = new User();
+        sampleUser.setId("id");
+        sampleUser.setUsername("jhon");
+        sampleUser.setPassword("thor");
+        Mockito.when(userRepository.findById("id")).thenReturn(Optional.of(sampleUser));
         userService.findUserById("id");
-        Mockito.verify(userRepository, Mockito.times(1)).findById("id");
+        Mockito.verify(userRepository, Mockito.times(2)).findById("id");
     }
 
     @Test
@@ -47,8 +55,9 @@ class UserServiceImplTest {
         sampleUser.setId("id");
         sampleUser.setUsername("jhon");
         sampleUser.setPassword("thor");
+        Mockito.when(userRepository.findById("id")).thenReturn(Optional.of(sampleUser));
         userService.updateUser(sampleUser);
-        Mockito.verify(userService, Mockito.times(2)).findUserById("id");
+        Mockito.verify(userService, Mockito.times(1)).findUserById("id");
     }
 
     @Test
@@ -57,12 +66,20 @@ class UserServiceImplTest {
         sampleUser.setId("id");
         sampleUser.setUsername("jhon");
         sampleUser.setPassword("thor");
-
+        Mockito.when(userRepository.findById("id")).thenReturn(Optional.of(sampleUser));
+        Mockito.when(userService.saveUser(sampleUser)).thenReturn(sampleUser);
         userService.updateUser(sampleUser);
-        Mockito.verify(userService, Mockito.times(1)).saveUser(sampleUser);
+        Mockito.verify(userService, Mockito.times(1)).updateUser(sampleUser);
     }
 
     @Test
-    void deleteUserById() {
+    void deleteUserById_should_call_userRepository_delete_once() {
+        User sampleUser = new User();
+        sampleUser.setId("id");
+        sampleUser.setUsername("jhon");
+        sampleUser.setPassword("thor");
+        Mockito.when(userRepository.findById("id")).thenReturn(Optional.of(sampleUser));
+        userService.deleteUserById("id");
+        Mockito.verify(userRepository, Mockito.times(1)).delete(sampleUser);
     }
 }
