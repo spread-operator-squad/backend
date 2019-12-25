072f1c9e7d33bea084db20e384495504c2a8e54f
Wahyudin <whydin.id@hotmail.com>
2019-12-25 22:37:07 +0700

    [IMPROVE] add validation empty field for updating user

--- src/main/java/com/enigma/controller/UserController.java
+++ src/main/java/com/enigma/controller/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
     }
 
     @PutMapping
-    public User updateUser(@RequestBody User user){
+    public User updateUser(@Valid @RequestBody User user){
         return this.userService.updateUser(user);
     }
 
